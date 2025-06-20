b6af96c7fb68498eeb0757fd5d8eb152053a88a8
aadriien <adriienlynch@gmail.com>
2025-06-20 17:30:44 -0400

    Refactored initial function to work flexibly with part 2 solution

--- 2017-day6-memoryReallocation.py
+++ 2017-day6-memoryReallocation.py
@@ -14,12 +14,14 @@ def read_file(filename: str) -> list[int]:
         return [int(bank) for bank in banks_strs]
 
 
-# Part 1 of problem
-def unique_redistributions(banks: list[int]) -> int:
-    seen = []
+def unique_redistributions(banks: list[int], part: int) -> int:
+    seen = {}
     cycles = 0
 
-    def redistribute_new(banks: list[int], seen: list[list[int]]) -> bool:
+    while tuple(banks) not in seen:
+        # Record configuration (curr banks as key & count of steps as value)
+        seen[tuple(banks)] = cycles
+
         # Step 1: get index with max blocks (will default to 1st seen if tie)
         max_blocks = max(banks)
         max_blocks_idx = banks.index(max_blocks)
@@ -34,25 +36,17 @@ def unique_redistributions(banks: list[int]) -> int:
             max_blocks -= 1
             i = (i + 1) % mod_base
 
-        # Step 3: record configuration
-        if banks in seen:
-            return True
-        else:
-            # Store copy to avoid issues with mutating array
-            seen.append(banks[:])
-            return False
-
-    while not redistribute_new(banks, seen):
         cycles += 1
 
-    return cycles + 1
-
+    # In the case of part 2, we want index distance
+    return cycles if part == 1 else cycles - seen[tuple(banks)]
 
 
 if __name__ == "__main__":
     banks = read_file(filename=INPUT_FILE)
 
-    cycles = unique_redistributions(banks=banks)
+    cycles = unique_redistributions(banks=banks[:], part=1)
+    second_cycles = unique_redistributions(banks=banks[:], part=2)
 
-    print(f"redistribution cycles: {cycles} ... redistribution cycles: {cycles}")
+    print(f"redistribution cycles: {cycles} ... second cycles: {second_cycles}")
 
