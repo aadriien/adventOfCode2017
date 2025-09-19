1e47c065754484d5569d95514bbdad852ba21726
aadriien <adriienlynch@gmail.com>
2025-09-18 23:07:43 -0400

    Finished part 2 of day 15 exercise

--- 2017-day15-duelingGenerators.py
+++ 2017-day15-duelingGenerators.py
@@ -25,9 +25,15 @@ def read_file(filename: str) -> tuple[int, int]:
     return generator_vals[0], generator_vals[1]
 
 
-def generate_next(prev_val: int, factor: int) -> int:
-    product = prev_val * factor
-    return product % DIVISOR
+def generate_next(prev_val: int, factor: int, other_divisor: int = 1) -> int:
+    while True:
+        product = prev_val * factor
+        remainder = product % DIVISOR
+        
+        if remainder % other_divisor == 0:
+            return remainder
+        
+        prev_val = remainder
 
 
 def pairs_match(value_A: int, value_B: int) -> bool:
@@ -51,19 +57,37 @@ def get_final_count(starting_A: int, starting_B: int) -> int:
     return final_count
     
 
+# Part 2 of problem
+def get_selective_count(starting_A: int, starting_B: int) -> int:
+    selective_count = 0
+
+    # Generate the 5 mil pairs
+    for _ in range(5000000):
+        starting_A = generate_next(starting_A, FACTOR_A, 4)
+        starting_B = generate_next(starting_B, FACTOR_B, 8)
+
+        if pairs_match(starting_A, starting_B):
+            selective_count += 1
+
+    return selective_count
+    
+
 # Validate examples with unit tests
 def run_tests() -> None:
     # Test part 1
     assert (get_final_count(65, 8921) == 588)
+
+    # Test part 2
+    assert (get_selective_count(65, 8921) == 309)
     
 
 if __name__ == "__main__":
     run_tests()
     
     starting_A, starting_B = read_file(filename=INPUT_FILE)
-    print(starting_A, starting_B)
 
     final_count = get_final_count(starting_A=starting_A, starting_B=starting_B)
+    selective_count = get_selective_count(starting_A=starting_A, starting_B=starting_B)
 
-    print(f"final count: {final_count} ... ")
+    print(f"final count: {final_count} ... selective count: {selective_count}")
 
